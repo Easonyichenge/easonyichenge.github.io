1fe81eda197b0fbc501ea55cf9cb168399d1d1c1
Eason Shen <106938239+Easonyichenge@users.noreply.github.com>
2025-12-03 21:11:02 +0800

    Delete weather directory

--- weather/check_ci.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const data = JSON.parse(fs.readFileSync('response.json', 'utf8'));
-const ciElement = data.records.Locations[0].Location[0].WeatherElement.find(e => e.ElementName === '舒適度指數');
-console.log(JSON.stringify(ciElement.Time[0], null, 2));
--- weather/check_elements.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const data = JSON.parse(fs.readFileSync('response.json', 'utf8'));
-const elements = data.records.Locations[0].Location[0].WeatherElement;
-elements.forEach(e => console.log(e.ElementName));
